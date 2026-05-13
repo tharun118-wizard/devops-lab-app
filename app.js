1949b7046abc8a66b62a3079cb77db6f428f8efe
Tharun Gowda K <tharungowdak575@gmail.com>
2026-05-13 13:09:14 +0000

    changed app.js comment

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require('express');
-const app     = express();
-const PORT    = process.env.PORT || 3000;
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({
-    message: "Hello from KTG's Jenkins CI/CD!",
-    version: process.env.APP_VERSION || '1.0.0',
-    buildNumber: process.env.BUILD_NUMBER || 'local',
-    timestamp: new Date().toISOString()
+  res.status(200).json({
+    message: 'Hello from DevOps Lab App'
   });
 });
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'UP' });
+  res.status(200).json({
+    status: 'UP'
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
-});
+// ONLY start server if file is run directly
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+  });
+}
 
 module.exports = app;
